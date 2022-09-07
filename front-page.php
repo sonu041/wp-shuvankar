e896a893639f9430d06f9bdd1b381e6b011ffc37
Shuvankar Sarkar <sonu041@gmail.com>
2022-09-07 09:59:09 +0530

    Added Google Cloud Certification

--- front-page.php
+++ front-page.php
@@ -79,6 +79,7 @@
 					<li><a data-weight="3" href="#tools">VBA</a></li>
 					<li><a data-weight="5" href="#tools">SQLServer</a></li>
 					<li><a data-weight="7" href="#tools">NewRelic</a></li>
+					<li><a data-weight="3" href="#tools">GoogleCloud</a></li>
 				</ul>
 			</div>
 			<div class="col-lg-12 text-right">*Level of expertise depends on the size of the text</dev>
@@ -93,6 +94,16 @@
 		<h2 class="display-2 text-center font-8vw">Certification</h2>
 		<div class="row pt-3 pb-3">
 			<div class="col-lg-3 text-center">
+				<a target="_blank" href="#">
+					<img src="images/GCP-ACE_Logo.png" alt="GCP-ACE" width="200" height="200">
+				</a>
+			</div>
+			<div class="col-lg-3 text-center">
+				<a target="_blank" href="https://www.credential.net/cdbcd9f7-513a-4899-976d-5dc795e6a2be">
+					<img src="images/GCP-CDL_Logo.png" alt="GCP-CDL" width="200" height="200">
+				</a>
+			</div>
+			<div class="col-lg-3 text-center">
 				<a target="_blank" href="https://catalog-education.oracle.com/pls/certview/sharebadge?id=854A64A9373B6EC1042CB5B34F947F5C8F30B5484CB9609E6CE4454C7143A332">
 					<img src="images/OCI_Foundation_2021.jpg" alt="OCI_Foundation" width="200" height="200">
 				</a>
@@ -102,6 +113,9 @@
 					<img src="images/BADGES_FINAL_PSM-I_600.png" alt="PSM-I" width="200" height="200">
 				<a>
 			</div>
+			
+		</div>
+		<div class="row pt-3 pb-3">
 			<div class="col-lg-3 text-center">
 				<a target="_blank" href="https://aspen.eccouncil.org/VerifyBadge?type=certification&a=wROWxqVXfkMwU0h7ibS0swOpqP2BFvcry9buh1Xkvxc=">
 					<img src="images/CEH_2E345519D3F7.png" alt="CEH" width="200" height="200">
@@ -112,10 +126,6 @@
 					<img src="images/Google-Play.png" alt="Google-Play" width="200" height="200">
 				</a>
 			</div>
-		</div>
-		<div class="row pt-3 pb-3">
-			<div class="col-lg-3 text-center">
-			</div>
 			<div class="col-lg-3 text-center">
 				<a target="_blank" href="https://www.credly.com/badges/aebbb0d9-015a-4878-9ddd-373fe63abe6e/public_url">
 					<img src="images/MCSD_App_Builder_2017-01.png" alt="MCSD" width="200" height="200">
@@ -125,8 +135,6 @@
 				<a target="_blank" href="https://www.credly.com/badges/5e6734d5-cf66-4342-bda2-573e89ca0fe5/public_url">
 					<img src="images/MCSA_Web_Applications_2017-01.png" alt="MCSA" width="200" height="200">
 				</a>
-			</div>
-			<div class="col-lg-3 text-center">
 			</div>
 		</div>
 	</div>
